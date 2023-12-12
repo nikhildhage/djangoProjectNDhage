9b36d62aee5e98c2d94b4727011e25854fde0f3f
nikhildhage <nikhil.dhage@gmail.com>
2023-12-12 15:52:53 -0800

    import sendgrid mail helper

--- pacificTrailsResort/views.py
+++ pacificTrailsResort/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import sendgrid
+import os
+from sendgrid.helpers.mail import *
+
 
 # Create your views here.
-
+# Index view
 def index(request):
-    return HttpResponse("Hellow World! You ar aat Pacific Trails Resort Index")
+    return render(request, "index.html")
+
+
+# Yurts view
+def yurts(request):
+    return render(request, "yurts.html")
+
+
+# Activities View
+def activities(request):
+    return render(request, "activities.html")
+
+
+# Reservations View
+def reservations(request):
+    return render(request, "reservations.html")
+
+
+# Contact View
+def contact(request):
+    return render(request, "contact.html")
