0d421d83070097e3be65a72dc89dccb1b556b876
nikhildhage <nikhil.dhage@gmail.com>
2023-12-12 21:22:06 -0800

    Updated  App urls to include details page

--- pacificTrailsResort/urls.py
+++ pacificTrailsResort/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-# App Urls
+# Pacific Trails Resort Urls
 urlpatterns = [
     path("", views.index, name="index"),
     path("yurts", views.yurts, name="yurts"),
