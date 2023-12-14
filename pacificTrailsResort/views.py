8e0ccc103a9c2e0f0f7498ef89b37871f0a6a2a4
nikhildhage <nikhil.dhage@gmail.com>
2023-12-14 01:27:28 -0600

    implemented sendgrid api code

--- pacificTrailsResort/views.py
+++ pacificTrailsResort/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 import sendgrid
 import os
 from sendgrid.helpers.mail import *
+from sendgrid import SendGridAPIClient
 
 
 # Create your views here.
@@ -24,6 +24,22 @@ def activities(request):
 
 # Reservations View
 def reservations(request):
+    print("nothing")
+    if request.method == 'POST':
+        message = Mail(
+            from_email='nikhil.dhage@gmail.com',
+            to_emails='shivba.d@gmail.com',
+            subject='Pacific Trails Resort',
+            html_content='<strong>Hello from Pacific Trails Resort</strong>')
+        try:
+            sg = SendGridAPIClient(os.getenv('SENDGRID_API_KEY'))
+            response = sg.send(message)
+            print(response.status_code)
+            print(response.body)
+            print(response.headers)
+        except Exception as e:
+            print(e.message)
+
     return render(request, "reservations.html")
 
 
