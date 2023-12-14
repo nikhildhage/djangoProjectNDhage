995fcb4d2d7b95f2078f78fac93a58bda0391037
nikhildhage <nikhil.dhage@gmail.com>
2023-12-13 20:30:24 -0600

    added models to model.py

--- pacificTrailsResort/models.py
+++ pacificTrailsResort/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    user_name = models.CharField(max_length=100)
+    user_password = models.CharField(max_length=100)
+    user_email = models.EmailField(unique=True)
+    reservation = models.ForeignKey('Reservation', on_delete=models.SET_NULL, null=True)
+
+
+class Reservation(models.Model):
+    reservation_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+    arrival_date = models.DateField()
+    nights = models.IntegerField()
